35f54e36686528bbefcaf0f0740ed2937c4983e8
kaushikgub <kaushik.gub@gmail.com>
2021-03-09 22:55:57 +0600

    first commit

---
+++ LinkedList.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LinkedList
+{
+    class LinkedList
+    {
+        private class Node
+        {
+            //private int value;
+            public int value { get; set; }
+            public Node next { get; set; }
+
+            public Node(int value)
+            {
+                this.value = value;
+            }
+        }
+
+
+        // Implementing LinkedList
+        private Node head;
+        private Node tail;
+
+        // Add Last
+        public void AddLast(int value)
+        {
+            Node node = new Node(value);
+            if(head == null)
+            {
+                head = tail = node;
+            } 
+            else
+            {
+                tail.next = node;
+                tail = node;
+            }
+        }
+
+        // Print LinkedList
+        public string print()
+        {
+            string list = "";
+            Node current = head;
+            while (current.next != null)
+            {
+                list += current.value + " ";
+                current = current.next;
+            }
+            return list;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace LinkedList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            LinkedList linkedList = new LinkedList();
+            linkedList.AddLast(10);
+            linkedList.AddLast(20);
+            linkedList.AddLast(30);
+
+            Console.WriteLine(linkedList.print());
+        }
+    }
+}
